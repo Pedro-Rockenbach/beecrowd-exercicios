fcbe99cc7c2ffa0d883e25b65f94c4b43920cc32
Pedro-Rockenbach <pedrorbach@gmail.com>
2026-04-23 21:01:35 -0300

    Voltando a resolver: foco cpp

---
+++ 2764/2784.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+  string data{};
+  cin >> data;
+
+  cout << data[3] << data[4] << "/" << data[0] << data[1] << "/" << data[6] << data[7] << endl;
+  cout << data[6] << data[7] << "/" << data[3] << data[4] << "/" << data[0] << data[1] << endl;
+  cout << data[0] << data[1] << "-" << data[3] << data[4] << "-" << data[6] << data[7] << endl;
+}
